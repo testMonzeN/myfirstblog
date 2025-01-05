dad9f3a4c5fa4126ef95251085ccaabd8a9baaf7
karadev <118184934+testMonzeN@users.noreply.github.com>
2025-01-05 18:08:28 +0500

    'ddddd'

--- blog/static/js/main.js
+++ blog/static/js/main.js
@@ -1,27 +1,29 @@
-    function loadPage(page) {
-        fetch('blog/?page=${page}')
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Ошибка загрузки страницы');
-                }
-                return response.text();
-            })
-            .then(html => {
+function loadPage(page) {
+    fetch('blog/?page=' + page)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Ошибка загрузки страницы');
+            }
+            return response.text();
+        })
+        .then(html => {
             try {
-                document.getElementById('pagination-links').innerHTML = html;
+                const paginationLinks = document.getElementById('pagination-links');
+                if (paginationLinks) {
+                    paginationLinks.innerHTML = html;
+                } else {
+                    console.error('нету элемента с id "pagination-links"');
+                }
             } catch (err) {
                 console.log('error - ', err);
-            }})
-            .catch(error => {
-                console.error(error);
-            });
-    }
+            }
+        })
+        .catch(error => {
+            console.error(error);
+        });
+}
 
-
-
-    document.addEventListener("DOMContentLoaded", function() {
-        loadPage(1);
-
-        document.getElementById('pagination-links').addEventListener('click', function(e) {
-            console.log('111111111111111')
-})})
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('start')
+    loadPage(1);
+});
